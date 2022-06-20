91496d0f08371dd1bb4877f293aa20d7db7fc6fd
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-06-20 21:51:57 +0300

    chore(cors): cors policy.

--- app/server/middleware/cors.go
+++ app/server/middleware/cors.go
@@ -5,14 +5,14 @@ import (
 	"quote/api/app/tools/logger"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/gin-contrib/cors"
 )
 
 func NewCORSMiddleware(cors config.CorsConfig) Middleware {
 	log := logger.NewLogger("cors-log")
 	return func(context *gin.Context) {
 		log.Warnf("CORS Header is enabled & set to: %s", cors.AllowedOrigins)
-		context.Header("Access-Control-Allow-Origin", cors.AllowedOrigins)
+		// context.Header("Access-Control-Allow-Origin", cors.AllowedOrigins)
+		context.Header("Access-Control-Allow-Origin", "*")
 		context.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		context.Header("Access-Control-Allow-Methods", "HEAD, GET, POST, DELETE, PUT, OPTIONS")
 	}
